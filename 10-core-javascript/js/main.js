2ccda10014c50e7567cf9621de528e1439c9a359
Brazatanu-Borah <136308834+Brazatanu-Borah@users.noreply.github.com>
2026-09-23 19:51:51 +0530

    added the core javascript styudy notes - part i. javascript history, variables, linking html to js, basic operations, arrays, conditionals and loops

---
+++ 10-core-javascript/js/main.js
@@ -0,0 +1,6 @@
+
+// External JavaScript File is basically a text file with a '.js' file extension
+
+console.log('JavaScript, from external js file');
+
+console.log('JavaScript, again!, from external js file');
